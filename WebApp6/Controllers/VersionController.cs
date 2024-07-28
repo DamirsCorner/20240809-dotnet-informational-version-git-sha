8bfa84dd70eaaee5203f319fd86b89c3d9b3efb9
Damir Arh <damir.arh@gmail.com>
2024-07-28 19:59:40 +0200

    Add project files.

---
+++ WebApp6/Controllers/VersionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp6.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VersionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var informationalVersion = Assembly
+            .GetExecutingAssembly()
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            ?.InformationalVersion;
+
+        return Ok(new { informationalVersion });
+    }
+}
